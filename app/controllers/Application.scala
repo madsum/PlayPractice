3b971122e6559ca077a842dc10d8f0d7b8538781
Masum Islam <madsum@gamil.com>
2015-02-07 12:41:06 +0200

    Do some more from win conflict for face.Do some more conflict

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,7 +5,7 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("This is from win conflicrt branch"))
+    Ok(views.html.index("This is from win conflicrt branch. Again do some update from win conflict"))
   }
 
 }
